dc4882bc6d9a3b919e20fcbbdc3b43128490fa23
ChristianAlameda <jazzbilbo@gmail.com>
2023-05-20 12:28:15 -0700

    added some basic implementations for me to fill in later

--- settingUp.py
+++ settingUp.py
@@ -2,3 +2,19 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 print(myclient.list_database_names())
+
+class inventory:
+    def __init__(self):
+        pass
+    def add(self,itemName):
+        pass
+    def remove(self, itemName):
+        pass
+    def edit(self, itemName, newItemName):
+        pass
+class peopleCounting:
+    def __init__(self):
+        pass
+    #count the ammount of people that were there each day and keep that for each day
+    def countPerDay(self):
+        pass
