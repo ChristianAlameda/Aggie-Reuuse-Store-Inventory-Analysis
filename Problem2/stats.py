86fa5c355f87b9e836611f792c227ff3eb6422e4
faveingpear <MPearsonpear@protonmail.com>
2023-05-20 23:34:25 -0700

    Stats now outputs a  products csv

--- Problem2/stats.py
+++ Problem2/stats.py
@@ -16,8 +16,23 @@ def main():
         except KeyError:
             pass
 
-    data_file = open("stats.csv", "w")
+    data_file = open("IDs.csv", "w")
     writer = csv.writer(data_file)
+
+    allPosts2 = []
+    for post in db.returnPosts():
+        try:
+            allPosts2.append({
+                "item": post["item"],
+                "picture": post["picture"],
+                "qrcode": post["qrcode"],
+                "addedItemDescription": post["addedItemDescription"]
+            })
+        except KeyError:
+            pass
+
+    data_file2 = open("Products.csv", "w")
+    writer2 = csv.writer(data_file2)
 
     count = 0
 
@@ -30,6 +45,18 @@ def main():
         writer.writerow(value.values())
 
     data_file.close()
+
+    count2 = 0
+
+    for value in allPosts2:
+        if count2 == 0:
+            header = value.keys()
+            writer2.writerow(header)
+            count2 += 1
+        
+        writer2.writerow(value.values())
+
+    data_file2.close()
 if __name__ == "__main__":
     main()
 
