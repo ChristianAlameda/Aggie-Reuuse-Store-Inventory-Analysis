83c9964f281a1519114705b2a5043b1790053e08
ChristianAlameda <jazzbilbo@gmail.com>
2023-05-21 08:49:28 -0700

    added picture :)

--- Problem2/ui.py
+++ Problem2/ui.py
@@ -1,42 +1,3 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
- 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(800, 800)
-        
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(150, 70, 93, 28))
- 
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(130, 149, 151, 31))
-        self.label.setText("")
- 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-        
-        # adding signal and slot
-        self.pushButton.clicked.connect(self.showmsg)
- 
-        
-        
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(150, 70, 93, 28))
-        #self.getText = 
-        #self.getItem()
-        
-        
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Click"))
-         
-    def showmsg(self):
-        # slot
-        self.label.setText("You clicked me")
-
-
 # importing required libraries
 from PyQt5.QtWidgets import *
 from PyQt5.QtMultimedia import *
@@ -244,22 +205,12 @@ class MainWindow(QMainWindow):
   
 # Driver code
 if __name__ == "__main__" :
-    app = QtWidgets.QApplication(sys.argv)
- 
- 
-      # create pyqt5 app
-    App = QApplication(sys.argv)
+    
+  # create pyqt5 app
+  App = QApplication(sys.argv)
   
   # create the instance of our Window
-    window = MainWindow()
+  window = MainWindow()
   
   # start the app
-    
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
- 
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
-
+  sys.exit(App.exec())
